9abac3dc2b2a54b4340a21b2583ff9c162817c64
Nadia Abby <jones.nadia.l@gmail.com>
2020-04-10 16:53:32 -0400

    working_4/10
    Render of one card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,18 @@
 import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
 
-function App() {
+function App(props) {
+  props = contacts;
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Card
+        name="Beyonce"
+        imgURL="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="+123 456 789"
+        email="b@beyonce.com"
+      />
     </div>
   );
 }
