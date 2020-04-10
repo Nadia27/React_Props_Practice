7159a036856630d9b1b0e771f5046c474943423b
Nadia Abby <jones.nadia.l@gmail.com>
2020-04-10 19:50:25 -0400

    working_4/10
    Remove hard-coded card. Card shown via contact.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
-function App(props) {
-  props = contacts;
+function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Card
-        name="Beyonce"
-        imgURL="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        tel="+123 456 789"
-        email="b@beyonce.com"
+        name={contacts[0].name}
+        imgURL={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
       />
     </div>
   );
